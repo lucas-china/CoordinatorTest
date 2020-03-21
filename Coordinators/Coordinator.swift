daf71e68a5c9551340430620dc2df0291df3cae5
Lucas <lucasbrito_9@hotmail.com>
2020-03-21 16:37:26 -0300

    Created Coordinator protocol.

---
+++ Coordinators/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  Coordinators
+//
+//  Created by Lucas Santana Brito on 21/03/20.
+//  Copyright © 2020 lsb. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationControllers: UINavigationController { get set }
+    
+    func start()
+}
---
+++ Coordinators/Storyboarded.swift
@@ -0,0 +1,9 @@
+//
+//  Storyboarded.swift
+//  Coordinators
+//
+//  Created by Lucas Santana Brito on 21/03/20.
+//  Copyright © 2020 lsb. All rights reserved.
+//
+
+import Foundation
