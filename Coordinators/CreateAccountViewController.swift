2c19d16e9d4d2c5f558da529cd56a22af60f2e09
Lucas <lucasbrito_9@hotmail.com>
2020-03-21 17:14:58 -0300

    added textfields in CreateAccount and WelcomeView.

--- Coordinators/CreateAccountViewController.swift
+++ Coordinators/CreateAccountViewController.swift
@@ -10,23 +10,17 @@ import UIKit
 
 class CreateAccountViewController: UIViewController, Storyboarded {
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     weak var coordinator: MainCoordinator?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func tapCreate(_ sender: UIButton) {
     }
-    */
-
+    
 }
